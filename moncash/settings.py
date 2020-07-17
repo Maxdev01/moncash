8aa6b25aceef12c3ad42ad1c1a3baeca8fd8e064
Maxdev01 <61092309+Maxdev01@users.noreply.github.com>
2020-07-17 12:35:12 -0700

    conf

--- moncash/settings.py
+++ moncash/settings.py
@@ -120,9 +120,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 import django_heroku
 
 django_heroku.settings(locals())
